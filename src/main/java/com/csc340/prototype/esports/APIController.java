c3e86cae5a34d4303ae51aca033530b5f0e64d8c
Arsal Zubair <akzubair@uncg.edu>
2024-10-01 12:45:23 -0400

    Initial Push

---
+++ src/main/java/com/csc340/prototype/esports/APIController.java
@@ -0,0 +1,36 @@
+package com.csc340.prototype.esports;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@org.springframework.stereotype.Controller
+public class APIController {
+
+
+    @GetMapping("/")
+    public String home() {
+        return "discussion_hub.html";
+    }
+
+    @GetMapping("/Index.html")
+    public String redirectToExternalUrl() {
+        return "redirect:http://localhost:8080/index.html";
+    }
+
+    @GetMapping("/discussion")
+    public String discussion() {
+        return "discussion_hub.html";
+    }
+
+    @GetMapping("/teams")
+    public String teams() {
+        return "teams.html";
+    }
+
+    @GetMapping("/coaches")
+    public String coach() {
+        return "coach_hub.html";
+    }
+
+}
