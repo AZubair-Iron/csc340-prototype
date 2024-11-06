909a3d796a2de5f3aa8e083cbe6999882fa003af
Arsal Zubair <akzubair@uncg.edu>
2024-11-06 16:05:56 -0500

    Fixed Naming Issue

---
+++ src/main/java/com/spartan/esports/APIController.java
@@ -0,0 +1,37 @@
+package com.spartan.esports;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@org.springframework.stereotype.Controller
+public class APIController {
+
+
+    @GetMapping("/")
+    public String home() {
+        return "discussion_hub.html";
+    }
+
+    @GetMapping("/Index.html")
+    public String redirectToExternalUrl() {
+        return "redirect:http://localhost:8080/index.html";
+    }
+
+    @GetMapping("/discussion")
+    public String discussion() {
+        return "discussion_hub.html";
+    }
+
+    @GetMapping("/teams")
+    public String teams() {
+        return "teams.html";
+    }
+
+    @GetMapping("/coaches")
+    public String coach() {
+        return "coach_hub.html";
+    }
+
+}
+
--- src/main/java/com/spartan/esports/SpartanEsportsApplication.java
+++ src/main/java/com/spartan/esports/SpartanEsportsApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Assignment4Application {
+public class SpartanEsportsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Assignment4Application.class, args);
+		SpringApplication.run(SpartanEsportsApplication.class, args);
 	}
 
 }
